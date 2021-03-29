a035a05efcbd703072ced0372d58d75df7d9db5d
Max Coursey <mrc30363@gmail.com>
2021-03-29 13:26:51 -0400

    added some code to lb_pseduo for replica changes and topic distribution

--- src/lb_pseudo.py
+++ src/lb_pseudo.py
@@ -4,6 +4,7 @@ import threading
 import time
 import matplotlib.pyplot as plt
 import netifaces as ni
+from math import ceil
 from random import randrange, randint
 from kazoo.client import KazooClient
 from kazoo.client import KazooState
@@ -14,12 +15,13 @@ from kazoo.exceptions import (
     KazooException
 )
 
+TOPIC_THRESHOLD = 3
 context = zmq.Context()
 
 class LoadBalancer():
     
     def __init__(self):
-        
+        self.exist_replica_cnt = None
         
         def replica_sockets(self):
             #set up sockets REQ/RES to listen for a "become a primary" command
@@ -31,4 +33,38 @@ class LoadBalancer():
             #depending on where we store, how do we use to connect?
             
             
+        def lb_topics():
+            @self.zk.ChildrenWatch(self.topic_root_path)
+            def topic_watch(children):
+                print(f"Topic Children Change")
+                if children is not None:
+                    new_repl_cnt = ceil(len(children)/TOPIC_THRESHOLD)
+                    old_repl_cnt = self.zk.get_children(replica_root_path)
+                if old_repl_cnt != new_repl_cnt:
+                    update_replicas(new_repl_cnt)
+                else:
+                    print('Topics changed but count remained same')
+                    #skip over replica changes and check to see if we need to redistribute
+                    distribute_topics_to_replicas
+                    
+        def update_replicas(self, count):
+            replicas = self.zk.get_children(replica_root_path)
+            change_num = count - len(replicas)
+            
+            #create
+            if change_num > 0:
+                for _ in range(change_num):
+                    self.zk.create(replica_root_path, ephemeral=True, sequence=True, makepath=True, value="??") #How to get IP?
+            #delete
+            elif change_num < 0:
+                for i in range(-change_num):
+                    path = replica_root_path + "/" + replicas[i]
+                   self.zk.delete(path)
+                   print(f'Deleted replica {path}')
+          
+             distribute_topics_to_replicas
+                        
             #register dicts for pubs subs and brokers?
+        def distribute_topics_to_replicas():
+            topics = self.zk.get_children(self.topic_root_path)
+            
